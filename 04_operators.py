2614c4dd0e29318743be698315157061db457dce
Rutuja Anantrao Mhaske <rutujamhaske.2311@gmail.com>
2026-07-16 21:47:14 +0530

    Add files via upload

---
+++ 04_operators.py
@@ -0,0 +1,58 @@
+# Arithematic oprators
+
+a = 10
+b = 5
+c = a + b
+print(c)
+
+d = 35
+e = 14
+f = d - e
+print(f)
+
+g = 10
+h = 5
+i = g * h
+print(i)
+
+j = 10
+k = 5           
+l = j / k
+print(l)
+
+# Assignment operators
+
+a = 4 - 2  #Assign 4 - 2 in a
+print(a)
+b = 6
+#  b += 3 # Increment the value of b by 3 and then assign it to b
+#  b -= 3 # Decrement the value of b by 3 and then assign it to b
+#  b *= 3 # Multiply the value of b by 3 and then assign it to b
+b /= 3 # Divide the value of b by 3 and then assign it to b
+print(b)
+
+# Comparison operators
+
+# d = 5 >= 4 True
+# d = 5 != 5 False
+d = 10 == 10
+print(d)
+
+# Logical operators
+
+e = True or False
+
+# Truth table of or
+print("True or False is", True or False)
+print("True or True is", True or True)
+print("False or True is", False or True)
+print("False or False is", False or False)
+
+# Truth table of and
+print("True and False is", True and False)
+print("True and True is", True and True)     
+print("False and True is", False and True)
+print("False and False is", False and False)   
+
+print(not(False))  # True
+print(not(True))   # False
