f48b25c6daaa6a328f22dac9de23419ffb27aae9
Rutuja Anantrao Mhaske <rutujamhaske.2311@gmail.com>
2026-07-17 19:12:39 +0530

    Add files via upload

---
+++ 05_type.py
@@ -0,0 +1,20 @@
+# a = 31 
+# t = type(a) #class <int>
+
+# print(t)
+
+# a = 31.2
+# t = type(a) #class <float>
+
+# print(t)
+
+a = "31.2"
+b = float(a) # a but the type should be float
+z = type(b) #class <float>
+
+print(z)
+
+a = "Hello, World!"
+t = type(a) #class <str>
+
+print(t)
