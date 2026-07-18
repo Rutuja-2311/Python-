6c4db857474ac64824b6eba4e77bc9b468c7eaed
Rutuja Anantrao Mhaske <rutujamhaske.2311@gmail.com>
2026-07-18 19:18:38 +0530

    Add files via upload

---
+++ ch2_problem6.py
@@ -0,0 +1,5 @@
+a = int(input("Enter your number : "))
+
+print("the square of the number is", a ** 2) # valid
+# print("the square of the number is", a * a) valid
+# print("the square of the number is", a^2)   not valid
