17bb27fae41a421b8c60ff681795f8b29d4e58b6
Rutuja Anantrao Mhaske <rutujamhaske.2311@gmail.com>
2026-07-19 19:43:47 +0530

    Add files via upload

---
+++ ch3_03_slicing_with_skipvalue.py
@@ -0,0 +1,7 @@
+word = "amazing"
+
+print(word[1:7:2]) # start from index 1 all the way till 7 (excluding 7) with a skip value of 2 (Ans = mzn)
+
+print(word[:7]) # [0:7] amazing
+print(word[0:7]) # [0:7] amazing
+
