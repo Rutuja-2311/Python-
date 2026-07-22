0c61352b38be85ecb5fcdec1dfc973918066835b
Rutuja Anantrao Mhaske <rutujamhaske.2311@gmail.com>
2026-07-22 23:50:34 +0530

    Add files via upload

---
+++ ch4_02_list_methods.py
@@ -0,0 +1,13 @@
+friends  = ["Alice", "Bob", "Charlie", 5 , 4.87, False, "David", "Eva"]
+print(friends)
+friends.append("Frank")
+print(friends)
+
+l1 = [1, 34, 43, 53, 24, 12, 3]
+# l1.sort()    
+# l1.reverse()
+# l1.insert(3, 25) # Insert 25 such that its index in the list is 3
+# value = l1.pop(3) # Remove the value at index 3 and return it
+# print(value)
+l1.remove(24) # Remove the value 24 from the list
+print(l1)
