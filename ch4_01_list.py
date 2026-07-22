2927b4d3d7c1d561cd15ca2558693d54db04ee07
Rutuja Anantrao Mhaske <rutujamhaske.2311@gmail.com>
2026-07-22 23:32:07 +0530

    Add files via upload

---
+++ ch4_01_list.py
@@ -0,0 +1,7 @@
+friends  = ["Alice", "Bob", "Charlie", 5 , 4.87, False, "David", "Eva"]
+
+print(friends[0])  # Output: Alice
+friends[1] = "Bobby"  # Changing Bob to Bobby (unlike strings lists are mutable)
+
+print(friends[1])  
+print(friends[1:4])  
