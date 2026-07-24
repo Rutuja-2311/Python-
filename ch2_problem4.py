d366d359530bb95a39b3bc758a0b5190b5b3e633
Rutuja Anantrao Mhaske <rutujamhaske.2311@gmail.com>
2026-07-24 22:37:45 +0530

    Add comments for clarity on comparison logic
    Added comments to clarify the purpose of the code.

--- ch2_problem4.py
+++ ch2_problem4.py
@@ -1,3 +1,5 @@
+# Use comparison operator to find out whether a given variable a is greater then 'b' or not. Take a = 34 and b = 80.
+
 a = int(input("Enter number 1: "))
 b = int(input("Enter number 2: "))
 
