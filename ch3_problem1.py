91b8dece20024696101b051fe7ec6abace8dda6f
Rutuja Anantrao Mhaske <rutujamhaske.2311@gmail.com>
2026-07-24 22:47:55 +0530

    Add comment to clarify program functionality
    Added a comment to explain the purpose of the program.

--- ch3_problem1.py
+++ ch3_problem1.py
@@ -1,3 +1,6 @@
+# write a python program to display a user entered name followed by good afternoon using input() function.
+
+
 name = input("Enter your name: ")
 
 print(f"Good afternoon, {name}!")
