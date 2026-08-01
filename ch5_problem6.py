b9ca2d8987669e7d3853160e0b8dc3686d1718df
Rutuja Anantrao Mhaske <rutujamhaske.2311@gmail.com>
2026-08-01 21:07:24 +0530

    Add files via upload

---
+++ ch5_problem6.py
@@ -0,0 +1,25 @@
+# create an empty dictionary. Allow 4 freinds to enter their favourite language as value and use key as their names. Assume that names are unique.
+
+d = {}
+
+name = input("Enter Freinds name: ")
+lang = input("Enter language name: ")
+
+d.update({name: lang})
+
+name = input("Enter Freinds name: ")
+lang = input("Enter language name: ")
+
+d.update({name: lang})
+
+name = input("Enter Freinds name: ")
+lang = input("Enter language name: ")
+
+d.update({name: lang})
+
+name = input("Enter Freinds name: ")
+lang = input("Enter language name: ")
+
+d.update({name: lang})
+
+print(d)
