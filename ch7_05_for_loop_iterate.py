ca1ada2cf6774ad587492a2615d84ca49565425d
Rutuja Anantrao Mhaske <rutujamhaske.2311@gmail.com>
2026-08-06 19:29:55 +0530

    Add files via upload

---
+++ ch7_05_for_loop_iterate.py
@@ -0,0 +1,15 @@
+## for loop with lists
+#  l = [1, 3, 6, 76, 543, 7]
+
+# for i in l:
+#     print(i)
+
+## for loop with tuples
+# t = (1, 3, 6, 76, 543, 7)
+# for i in t:
+#     print(i)
+
+## for loop with strings
+s = "Harry"
+for i in s:
+    print(i)
