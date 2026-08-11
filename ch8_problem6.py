d27935cf5ab099378aa4657dc944013c85e4fda9
Rutuja Anantrao Mhaske <rutujamhaske.2311@gmail.com>
2026-08-11 23:03:16 +0530

    Add files via upload

---
+++ ch8_problem6.py
@@ -0,0 +1,8 @@
+# Write a python function which convert inches to cms.
+
+def inch_to_cms(inch):
+    return inch * 2.54
+
+n = int(input("Enter value in inches: "))
+
+print(f"The corrosponding value in cms is {inch_to_cms(n)}")
