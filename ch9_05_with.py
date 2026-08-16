c42a92989cc6d77e904665dceb491a6c1d2539e5
Rutuja Anantrao Mhaske <rutujamhaske.2311@gmail.com>
2026-08-16 20:27:23 +0530

    Change file name to 'ch9_file.txt' in example
    Updated file name from 'file.txt' to 'ch9_file.txt' in both open statements.

--- ch9_05_with.py
+++ ch9_05_with.py
@@ -1,11 +1,11 @@
-f = open("file.txt")
+f = open("ch9_file.txt")
 
 print(f.read())
 
 f.close()
 
 # the same can be written using with statement like this:
-with open("file.txt") as f:
+with open("ch9_file.txt") as f:
     print(f.read())
 
 # you dont have to explicitely close the file 
