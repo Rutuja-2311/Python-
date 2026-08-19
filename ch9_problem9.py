283f11f4be3e752dc76ef3c0dbd7a786deaf8218
Rutuja Anantrao Mhaske <rutujamhaske.2311@gmail.com>
2026-08-19 21:27:29 +0530

    Add files via upload

---
+++ ch9_problem9.py
@@ -0,0 +1,13 @@
+# write a program to find out whether a file is identical and matches the content of another file.
+
+with open("ch9_f1.txt") as f:
+    content1 = f.read()
+
+with open("ch9_f2.txt") as f:
+    content2 = f.read()
+
+if(content1 == content2):
+    print("Yes these files are identical ")
+
+else:
+    print("No these files are not identical")
